05879aaf0246decf83d4e0c31ce814eb01bbedf2
maks <masksnytko@users.noreply.github.com>
2017-12-03 21:07:13 +0300

    optimaze

--- index.js
+++ index.js
@@ -1,42 +1,65 @@
 class Events {
     constructor() {
-        this._events = new Object;
+        this._events = {}
     }
     on(type, cb) {
-        let _cb = this._events[type];
-        if (_cb instanceof Function) {
-            this._events[type] = [_cb, cb]; 
-        } else if (_cb instanceof Array) {
-            _cb.push(cb);
+        const handler = this._events[type]
+        if (handler === undefined) {
+            this._events[type] = cb
         } else {
-            this._events[type] = cb;
+            if (typeof handler === 'function') {
+                this._events[type] = [handler, cb]
+            } else {
+                handler.push(cb)
+            }
         }
     }
     once(type, cb) {
-        let _cb = (...arg) => {
-            this.removeListener(type, _cb);
-            cb(...arg);
-        }
-        this.on(type, _cb);
-    }
-    emit(type, ...arg) {
-        let _cb = this._events[type];
-        if (_cb instanceof Function) {
-            _cb(...arg);
-        } else if (_cb instanceof Array) {
-            _cb.forEach(cb => cb(...arg));
-        }
+        this.on(type, function handler () {
+            this.removeListener(type, handler)
+            this._offset--
+            cb.apply(this, arguments)
+        })
     }
     removeListener(type, cb) {
-        let _cb = this._events[type];
-        if (_cb instanceof Function) {
-            delete this._events[type];
-        } else if (_cb instanceof Array) {
-            let index = _cb.indexOf(cb);
-            if (index !== -1) {
-                _cb.splice(index, 1);
+        let handler = this._events[type]
+
+        if (handler === undefined) {
+            return false
+        }
+
+        if (typeof handler === 'function') {
+            return delete this._events[type]
+        }
+
+        let index = handler.indexOf(cb)
+
+        if (index > -1) {
+            handler.splice(index, 1)
+            if (handler.length === 0) {
+                return delete this._events[type]
+            } else {
+                return true
             }
         }
+        return false
+    }
+    emit(type) {
+        const handler = this._events[type]
+        
+        if (handler === undefined) {
+            return false
+        }
+
+        if (typeof handler === 'function') {
+            handler.apply(this, arguments)
+        } else {
+            for (this._offset = 0; this._offset < handler.length; this._offset++) {
+                handler[this._offset].apply(this, arguments)
+            }
+        }
+
+        return true
     }
 }
 
