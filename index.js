e67624e4b5c7c81a38b13483c569d58edeb402bd
maks <masksnytko@users.noreply.github.com>
2017-12-06 11:04:53 +0300

    ;

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 class Events {
     constructor() {
-        this._events = {}
-        this._offset = 0
+        this._events = {};
+        this._offset = 0;
     }
     on(type, cb) {
-        const handler = this._events[type]
+        const handler = this._events[type];
         if (handler === undefined) {
-            this._events[type] = cb
+            this._events[type] = cb;
         } else {
             if (typeof handler === 'function') {
-                this._events[type] = [handler, cb]
+                this._events[type] = [handler, cb];
             } else {
-                handler.push(cb)
+                handler.push(cb);
             }
         }
     }
     once(type, cb) {
         this.on(type, function handler () {
-            this.removeListener(type, handler)
-            this._offset--
-            cb.apply(this, arguments)
+            this.removeListener(type, handler);
+            this._offset--;
+            cb.apply(this, arguments);
         })
     }
     removeListener(type, cb) {
-        let handler = this._events[type]
+        let handler = this._events[type];
 
         if (handler === undefined) {
-            return false
+            return false;
         }
 
         if (typeof handler === 'function') {
-            return delete this._events[type]
+            return delete this._events[type];
         }
 
-        let index = handler.indexOf(cb)
+        let index = handler.indexOf(cb);
 
         if (index > -1) {
-            handler.splice(index, 1)
+            handler.splice(index, 1);
             if (handler.length === 0) {
-                return delete this._events[type]
+                return delete this._events[type];
             } else {
-                return true
+                return true;
             }
         }
         
-        return false
+        return false;
     }
     emit(type, ...arg) {
         const handler = this._events[type];
         
         if (handler === undefined) {
-            return false
+            return false;
         }
 
         if (typeof handler === 'function') {
-            handler.apply(this, arg)
+            handler.apply(this, arg);
         } else {
             for (this._offset = 0; this._offset < handler.length; this._offset++) {
-                handler[this._offset].apply(this, arg)
+                handler[this._offset].apply(this, arg);
             }
         }
 
-        return true
+        return true;
     }
 }
 
